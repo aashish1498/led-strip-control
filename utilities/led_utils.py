109e893503ac01a0a7152fc6d0933343eaf1912f
Aashish Mehta <43656153+aashish1498@users.noreply.github.com>
2024-12-09 18:58:55 +0000

    chore: use exponent for red-green mapping

--- utilities/led_utils.py
+++ utilities/led_utils.py
@@ -3,7 +3,8 @@ from config import NUM_LEDS_TOTAL
 
 default_strip = apa102.APA102()
 my_cycle = None
-scale_factor = 255 / NUM_LEDS_TOTAL
+rgb_scale_factor = 255 / NUM_LEDS_TOTAL
+percentage_scale_factor = 100 / NUM_LEDS_TOTAL
 
 
 def hex_to_rgb(hex_code: str) -> tuple[int, int, int]:
@@ -11,14 +12,19 @@ def hex_to_rgb(hex_code: str) -> tuple[int, int, int]:
 
 
 def rainbow_colour_from_index(index: int) -> tuple[int, int, int]:
-    scaled_index = int(index * scale_factor)
+    scaled_index = int(index * rgb_scale_factor)
     return default_strip.wheel(scaled_index)
 
 
-def red_amber_green_from_index(index: int) -> tuple[int, int, int]:
-    scaled_index = int(index * scale_factor)
-    if scaled_index < 50:
-        return (255, 0, 0)
-    if 50 <= scaled_index < 85:
-        return (255, 255, 0)
-    return (0, 255, 0)
+def red_amber_green_from_index(index: int) -> any:
+    scaled_index = int(index * percentage_scale_factor)
+    green_value = exponential_map(scaled_index)
+    red_value = 255 - green_value
+    return default_strip.combine_color(red_value, green_value, 0)
+
+
+def exponential_map(value, input_min=0, input_max=100, output_min=0, output_max=255, exponent=2) -> int:
+    normalized = (value - input_min) / (input_max - input_min)
+    exp_value = normalized**exponent
+    mapped_value = output_min + (exp_value * (output_max - output_min))
+    return int(mapped_value)
