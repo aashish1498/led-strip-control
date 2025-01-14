7e8ec524aafbf0b8272b6d2fd0b677c489dcfe79
Aashish Mehta <43656153+aashish1498@users.noreply.github.com>
2025-01-14 02:02:14 +0000

    chore; improve descriptions

--- utilities/request_types.py
+++ utilities/request_types.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PercentageRequest(BaseModel):
@@ -7,9 +7,9 @@ class PercentageRequest(BaseModel):
 
 
 class SolidRequest(BaseModel):
-    hex_code: str
+    hex_code: str = Field(description="A list of hex codes for the colours to pulse through", examples=["#FF0000", "#00FF00", "#0000FF"])
 
 
 class PulseRequest(BaseModel):
-    colours: list[str]
+    colours: list[str] = Field(description="A list of hex codes for the colours to pulse through", examples=[["#FF00A6", "#00C8FF", "#0000FF"]])
     pause_time_seconds: float = 1
